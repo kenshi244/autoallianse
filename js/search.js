1052198a51fba8da12f068c0b0e992e33f717d4a
kenshi244 <maanukyaan@gmail.com>
2022-08-12 00:30:53 +0300

    Some changes on main page

--- js/search.js
+++ js/search.js
@@ -1,21 +1,15 @@
-let mainList = ["Привет", "Текст", "Какая-то фигня..."];
-
-let resultsDiv = document.getElementById("resultsDiv");
 let resultsContainer = document.querySelector(".search__results-container");
-let input = document.getElementById("mainSearch");
 let button = document.getElementById("searchDropdownButton");
 
-button.addEventListener("click", function () {
-    resultsContainer.classList.add("active");
-    resultsDiv.innerHTML = "";
-    for (let i = 0; i < mainList.length; i++) {
-        resultsDiv.innerHTML += "<li>" + mainList[i] + "</li>";
-    }
-});
+let isClosed = true;
 
-document.addEventListener("click", function (event) {
-    if(!button.contains(event.target)) {
-        resultsDiv.innerHTML = "";
+button.addEventListener("click", function () {
+    if(isClosed === true) {
+        resultsContainer.classList.add("active");
+        isClosed = false;
+    }
+    else {
         resultsContainer.classList.remove("active");
+        isClosed = true;
     }
 });
