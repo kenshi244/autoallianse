c236ac8efd823f59f6d960ea6b8971b0365d9858
kenshi244 <maanukyaan@gmail.com>
2022-09-12 19:25:54 +0300

    Registration form done

--- js/registration_validation.js
+++ js/registration_validation.js
@@ -1,13 +1,15 @@
 let registrationContainer = document.querySelector(".registration");
 let firstNextButton = document.querySelectorAll(".registration-button__next-step")[0];
 let secondNextButton = document.querySelectorAll(".registration-button__next-step")[1];
+let thirdNextButton = document.querySelectorAll(".registration-button__next-step")[2];
 let firstPrevButton = document.querySelectorAll(".registration-button__prev-step")[0];
 let secondPrevButton = document.querySelectorAll(".registration-button__prev-step")[1];
+let thirdPrevButton = document.querySelectorAll(".registration-button__prev-step")[2];
 let registrationStep = -1;
 let registrationSteps = document.querySelectorAll(".registration-step");
 
 firstNextButton.addEventListener("click", function (e) {
-    
+
     let registrationSelects = registrationContainer.querySelectorAll("select");
     let registrationEmail = document.querySelector("#registrationEmailInput");
     let firstCounter = 0;
@@ -88,6 +90,11 @@ secondNextButton.addEventListener("click", function (e) {
     nextStep();
 });
 
+thirdNextButton.addEventListener("click", function (e) {
+    registrationStep++;
+    nextStep();
+});
+
 firstPrevButton.addEventListener("click", function (e) {
     registrationStep--;
     prevStep();
@@ -98,10 +105,18 @@ secondPrevButton.addEventListener("click", function (e) {
     prevStep();
 });
 
+thirdPrevButton.addEventListener("click", function (e) {
+    registrationStep--;
+    prevStep();
+});
+
 let fieldsets = document.querySelectorAll("fieldset");
 
 function nextStep() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
     if (registrationStep === 0) {
         fieldsets[0].style.display = "none";
         fieldsets[1].style.display = "block";
@@ -114,11 +129,20 @@ function nextStep() {
 
         registrationSteps[1].classList.remove("active");
         registrationSteps[2].classList.add("active");
+    } else if (registrationStep === 2) {
+        fieldsets[2].style.display = "none";
+        fieldsets[3].style.display = "block";
+
+        registrationSteps[2].classList.remove("active");
+        registrationSteps[3].classList.add("active");
     }
 }
 
 function prevStep() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
     if (registrationStep === -1) {
         fieldsets[1].style.display = "none";
         fieldsets[0].style.display = "block";
@@ -131,5 +155,69 @@ function prevStep() {
 
         registrationSteps[2].classList.remove("active");
         registrationSteps[1].classList.add("active");
+    } else if (registrationStep === 1) {
+        fieldsets[3].style.display = "none";
+        fieldsets[2].style.display = "block";
+
+        registrationSteps[3].classList.remove("active");
+        registrationSteps[2].classList.add("active");
     }
 }
+
+let submitButton = document.querySelector("#submitButton");
+submitButton.addEventListener("click", function (e) {
+    let radios = document.querySelectorAll("input[name='yurLico']");
+    let selectedRadioOutput = radios[0].selected ? "юридическое лицо (ООО, ОАО, ЗАО)" : "индивидуальный предприниматель (ИП)";
+
+    let checkedCheckboxValue = document.querySelector("#nalogooblajenieCheckbox").checked ? "Является плательщиком НДС" : "Не является плательщиком НДС";
+    let inputValues = {
+        "username": document.querySelector("#registrationLogin").value,
+        "userEmail": document.querySelector("#registrationEmailInput").value,
+        "opf": selectedRadioOutput,
+        "country": document.querySelector("#country").innerHTML,
+        "region": document.querySelector("#registrationRegionSelect").options[document.querySelector("#registrationRegionSelect").selectedIndex].text,
+        "okrug": document.querySelector("#registrationOkrug").value,
+        "sfera": document.querySelector("#registrationSphereSelect").options[document.querySelector("#registrationSphereSelect").selectedIndex].text,
+        "source": document.querySelector("#registrationSourceSelect").options[document.querySelector("#registrationSourceSelect").selectedIndex].text,
+        "post": document.querySelector("#postInput").value,
+        "surname": document.querySelector("#surnameInput").value,
+        "name": document.querySelector("#nameInput").value,
+        "patronymic": document.querySelector("#patronymicInput").value,
+        "mainTelephone": document.querySelector("#mainTelephone").value,
+        "secondaryTelephone": document.querySelector("#secondaryTelephone").value,
+        "fax": document.querySelector("#fax").value,
+        "officeRoutine": document.querySelector("#officeRoutineInput").value,
+        "storageRoutine": document.querySelector("#storageRoutineInput").value,
+        "inn": document.querySelector("#innInput").value,
+        "kpp": document.querySelector("#kppInput").value,
+        "ogrn": document.querySelector("#ogrnInput").value,
+        "nalogooblajenie": checkedCheckboxValue,
+        "yurAdres": document.querySelector("#yurAdresInput").value,
+        "faktAdres": document.querySelector("#faktAdresInput").value,
+        "rs": document.querySelector("#rsInput").value,
+        "bik": document.querySelector("#bikInput").value,
+        "kor": document.querySelector("#korInput").value,
+        "naimenovanie": document.querySelector("#naimenovanieInput").value,
+        "headPosition": document.querySelector("#headPositionSelect").options[document.querySelector("#headPositionSelect").selectedIndex].text,
+        "headSurname": document.querySelector("#headSurnameInput").value,
+        "headName": document.querySelector("#headNameInput").value,
+        "headPatronymic": document.querySelector("#headPatronymicInput").value,
+        "kontaktDoljnost": document.querySelector("#kontaktDoljnostInput").value,
+        "kontaktSurname": document.querySelector("#kontaktSurnameInput").value,
+        "kontaktName": document.querySelector("#kontaktNameInput").value,
+        "kontaktPatronymic": document.querySelector("#kontaktPatronymicInput").value,
+        "kontaktTelephone": document.querySelector("#kontaktTelephone").value,
+        "kontaktEmail": document.querySelector("#kontaktEmail").value,
+    };
+
+    let registrationResults = document.querySelectorAll(".registration-result");
+    for (let i = 0; i < registrationResults.length; i++) {
+        for (const [index, [, value]] of Object.entries(Object.entries(inputValues))) {
+            if(+index === i) {
+            registrationResults[i].innerHTML = value;
+            break;
+            }
+        }
+    }
+
+});
